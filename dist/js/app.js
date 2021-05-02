9f4483ec112c0fa31176a28e6a64749405dc4b1c
equipoJmmm2021 <79748616+equipoJmmm2021@users.noreply.github.com>
2021-05-02 19:47:49 +0200

    Update app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,7 @@ Amplitude.init({
     "songs": [
         {
             "name": "Lily, Main theme",
-            "artist": "FlamingoStudios",
+            "artist": "FlamingoStudio",
             "album": "Lily",
             "url": "https://github.com/equipoJmmm2021/equipoJmmm2021.github.io/blob/main/loop.mp3?raw=true",
             "cover_art_url": "https://raw.githubusercontent.com/equipoJmmm2021/equipoJmmm2021.github.io/main/index/album.jpg"
